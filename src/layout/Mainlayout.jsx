54ba41835454ad80b53c8ba62d9525fba582662e
jonayet-nur <jonayetnur@gmail.com>
2026-04-20 01:49:53 +0600

    footer section added

--- src/layout/Mainlayout.jsx
+++ src/layout/Mainlayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/shared/Navbar/Navbar'
 import { Outlet } from 'react-router'
+import Footer from '../Components/Footer/Footer'
 
 const Mainlayout = () => {
   return (
     <div>
         <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
